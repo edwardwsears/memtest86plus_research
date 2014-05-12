a0e8eb342f65c11201a38baa4821dc28d3b74fa5
Edward Sears <edwardwsears@gmail.com>
2014-05-12 15:35:43 -0400

    added config, plot/stat choice, proper cmd line options

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ memtest86plus_research
 ======================
 
 Memtest86+ research CMU
+
+plot.py usage: 
+python plot.py -r <results_file> -c <config_file> -d <directory for graphs/stats>
+- directory must already be created
+
+python plot.py -h
+or
+python plot.py --help
+to display available options
--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 from matplotlib.backends.backend_pdf import PdfPages
+from optparse import OptionParser
 import sys
 import time
 
@@ -28,20 +29,20 @@ def create_statistics():
     file = open(output_folder+'/statistics.txt','w+')
     #DIMM stats
     file.write('DIMM Statistics\n')
-    for i in range(0,len(DIMM_array)):
-        perc = float(len(DIMM_array[i]))/float(total_errors)
+    for i in range(0,len(DIMM_counts)):
+        perc = float(DIMM_counts[i])/float(total_errors)
         file.write('DIMM'+str(i+1)+':'+str(perc)+'% of errors\n')
 
     #Bank stats
     file.write('\nBank Statistics\n')
-    for i in range(0,len(bank_array)):
-        perc = float(len(bank_array[i]))/float(total_errors)
+    for i in range(0,len(bank_counts)):
+        perc = float(bank_counts[i])/float(total_errors)
         file.write('Bank'+str(i+1)+':'+str(perc)+'% of errors\n')
 
     #Channel stats
     file.write('\nChannel Statistics\n')
-    for i in range(0,len(ch_array)):
-        perc = float(len(ch_array[i]))/float(total_errors)
+    for i in range(0,len(ch_counts)):
+        perc = float(ch_counts[i])/float(total_errors)
         file.write('Channel'+str(i+1)+':'+str(perc)+'% of errors\n')
 
     
@@ -71,6 +72,10 @@ def create_DIMMPlot(DIMM_array):
         plt.plot(page_array,offset_array,'r,',alpha=.7)
         page_array = []
         offset_array = []
+
+    #clean up
+    page_array = []
+    offset_array = []
 
     #show/save plot
     #plt.show()
@@ -107,6 +112,9 @@ def create_BankPlot(Bank_array):
         page_array = []
         offset_array = []
 
+    #clean up
+    page_array = []
+    offset_array = []
     #show/save plot
     #plt.show()
     print "%s%s%s" %("saving in ",output_folder,"/BankGraphs.pdf")
@@ -142,6 +150,9 @@ def create_chPlot(ch_array):
         page_array = []
         offset_array = []
 
+    #clean up
+    page_array = []
+    offset_array = []
     #show/save plot
     #plt.show()
     print "%s%s%s" %("saving in ",output_folder,"/ChannelGraphs.pdf")
@@ -154,7 +165,7 @@ def create_chPlot(ch_array):
 #return dictionary of items
 def parse_config():
     config_dict = {}
-    file_name = sys.argv[2]
+    file_name = options.config_file
     fileHandle = open(file_name,'r')
     file_str = fileHandle.read()
     fileHandle.close()
@@ -173,6 +184,14 @@ def parse_config():
 
 #MAIN
 t0 = time.clock()
+#parse cmdline flags
+parser = OptionParser()
+parser.add_option("-r","--results_file",action="store",type="string",dest="results_file",help="File that contains results of server errors")
+parser.add_option("-c","--config_file",action="store",type="string",dest="config_file",help="File that contains server configuration")
+parser.add_option("-d","--results_dir",action="store",type="string",dest="results_dir",help="Directory for results to be stored, directory must already be created")
+parser.add_option("-s","--stats_only",action="store_true",dest="stats_only",default=False,help="Only generate statistics (no graphs) *less memory intensive option*")
+parser.add_option("-g","--graphs_only",action="store_true",dest="graphs_only",default=False,help="Only generate graphs (no stats)")
+(options,args) = parser.parse_args()
 #parse config options
 config_dict = parse_config()
 #create plots
@@ -186,21 +205,34 @@ end_addr = 0
 stride = 0
 total_errors = 0
 
-#initialize arrays for graphs
+#initialize arrays for graphs and stats
+DIMM_counts = [0] * num_dimms
 DIMM_array = []
 for i in range(0,num_dimms):
     DIMM_array += [[]]
+bank_counts = [0] * num_banks
 bank_array = []
 for i in range(0,num_banks):
     bank_array += [[]]
+ch_counts = [0] * num_chs
 ch_array = []
 for i in range(0,num_chs):
     ch_array += [[]]
 
 #get output folder
-output_folder = sys.argv[3]
+output_folder = options.results_dir
 
-file_name = sys.argv[1]
+#set functionality
+create_graphs=True
+create_stats=True
+if (options.stats_only):
+    create_graphs=False
+    create_stats=True
+elif (options.graphs_only):
+    create_graphs=True
+    create_stats=False
+
+file_name = options.results_file
 fileHandle = open(file_name,'r')
 file_str = fileHandle.read()
 fileHandle.close()
@@ -218,17 +250,24 @@ for line in file_str:
         #run through range
         temp_addr = begin_addr
         while (temp_addr<=end_addr):
-            DIMM_array[choose_DIMM(temp_addr,config_dict)] += [temp_addr]
-            bank_array[choose_bank(temp_addr,config_dict)] += [temp_addr]
-            ch_array[choose_ch(temp_addr,config_dict)] += [temp_addr]
+            if (create_graphs):
+                DIMM_array[choose_DIMM(temp_addr,config_dict)] += [temp_addr]
+                bank_array[choose_bank(temp_addr,config_dict)] += [temp_addr]
+                ch_array[choose_ch(temp_addr,config_dict)] += [temp_addr]
+            if (create_stats):
+                DIMM_counts[choose_DIMM(temp_addr,config_dict)]+=1
+                bank_counts[choose_bank(temp_addr,config_dict)]+=1
+                ch_counts[choose_ch(temp_addr,config_dict)] +=1
             temp_addr += stride
 
 #create plots
-create_DIMMPlot(DIMM_array)
-create_BankPlot(bank_array)
-create_chPlot(ch_array)
+if (create_graphs):
+    create_DIMMPlot(DIMM_array)
+    create_BankPlot(bank_array)
+    create_chPlot(ch_array)
 
 #create statistics
-create_statistics()
+if (create_stats):
+    create_statistics()
 
 print "seconds to complete: ",time.clock()-t0
